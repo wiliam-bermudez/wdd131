fef2354b2075c26d6ebc0463cc533d00710ab8d0
wiliam-bermudez <wbermdezsols@byupathway.edu>
2025-07-09 19:10:59 -0600

    new

---
+++ script/getdates.js
@@ -0,0 +1,6 @@
+// Set current year
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
+    
